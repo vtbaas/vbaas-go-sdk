bd9d68d07d0df52c6a87ad28e8b8595f3ee88a6c
luongdv3 <luongdv3@viettel.com.vn>
2022-09-16 14:56:50 +0700

    update 0.0.3

--- go.mod
+++ go.mod
@@ -3,9 +3,11 @@ module github.com/vtbaas/vbaas-go-sdk
 go 1.17
 
 require (
+	github.com/golang/mock v1.6.0
 	github.com/hyperledger/fabric-protos-go-apiv2 v0.0.0-20220615102044-467be1c7b2e7
 	github.com/miekg/pkcs11 v1.1.1
 	github.com/stretchr/testify v1.7.1
+	google.golang.org/grpc v1.46.2
 	google.golang.org/protobuf v1.28.0
 )
 
@@ -17,7 +19,6 @@ require (
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20220527130721-00d5c0f3be58 // indirect
-	google.golang.org/grpc v1.46.2 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/client/identity_test.go
@@ -0,0 +1,89 @@
+/*
+Copyright 2020 IBM All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package client
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/vtbaas/vbaas-go-sdk/pkg/identity"
+	"github.com/vtbaas/vbaas-go-sdk/pkg/internal/test"
+	"github.com/hyperledger/fabric-protos-go-apiv2/gateway"
+	"github.com/hyperledger/fabric-protos-go-apiv2/msp"
+	"github.com/hyperledger/fabric-protos-go-apiv2/peer"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/protobuf/proto"
+)
+
+func TestIdentity(t *testing.T) {
+	privateKey, err := test.NewECDSAPrivateKey()
+	require.NoError(t, err)
+
+	certificate, err := test.NewCertificate(privateKey)
+	require.NoError(t, err)
+
+	id, err := identity.NewX509Identity("MSP_ID", certificate)
+	require.NoError(t, err)
+
+	serializedIdentity := &msp.SerializedIdentity{
+		Mspid:   id.MspID(),
+		IdBytes: id.Credentials(),
+	}
+	creator, err := proto.Marshal(serializedIdentity)
+	require.NoError(t, err)
+
+	t.Run("Evaluate uses client identity for proposals", func(t *testing.T) {
+		var actual []byte
+		mockClient := NewMockGatewayClient(gomock.NewController(t))
+		evaluateResponse := &gateway.EvaluateResponse{
+			Result: &peer.Response{
+				Payload: nil,
+			},
+		}
+		mockClient.EXPECT().Evaluate(gomock.Any(), gomock.Any()).
+			Do(func(_ context.Context, in *gateway.EvaluateRequest, _ ...grpc.CallOption) {
+				actual = test.AssertUnmarshalSignatureHeader(t, in.ProposedTransaction).Creator
+			}).
+			Return(evaluateResponse, nil).
+			Times(1)
+
+		contract := AssertNewTestContract(t, "contract", WithGatewayClient(mockClient), WithIdentity(id))
+
+		_, err := contract.EvaluateTransaction("transaction")
+		require.NoError(t, err)
+
+		require.EqualValues(t, creator, actual)
+	})
+
+	t.Run("Submit uses client identity for proposals", func(t *testing.T) {
+		var actual []byte
+		mockClient := NewMockGatewayClient(gomock.NewController(t))
+		endorseResponse := AssertNewEndorseResponse(t, "result", "channel")
+		statusResponse := &gateway.CommitStatusResponse{
+			Result: peer.TxValidationCode_VALID,
+		}
+		mockClient.EXPECT().Endorse(gomock.Any(), gomock.Any()).
+			Do(func(_ context.Context, in *gateway.EndorseRequest, _ ...grpc.CallOption) {
+				actual = test.AssertUnmarshalSignatureHeader(t, in.ProposedTransaction).Creator
+			}).
+			Return(endorseResponse, nil).
+			Times(1)
+		mockClient.EXPECT().Submit(gomock.Any(), gomock.Any()).
+			Return(nil, nil)
+		mockClient.EXPECT().CommitStatus(gomock.Any(), gomock.Any()).
+			Return(statusResponse, nil)
+
+		contract := AssertNewTestContract(t, "contract", WithGatewayClient(mockClient), WithIdentity(id))
+
+		_, err := contract.SubmitTransaction("transaction")
+		require.NoError(t, err)
+
+		require.EqualValues(t, creator, actual)
+	})
+}
